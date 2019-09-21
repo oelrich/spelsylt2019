09adad60f734b72c7c03d761d71e7552c18e741c
Johan Oelrich <johan.oelrich@vesicode.com>
2019-09-21 16:40:28 +0200

    Klartish ...

--- tutorial.lua
+++ tutorial.lua
@@ -13,9 +13,10 @@ Tutorial.energy = 1
 Tutorial.alive = true
 Tutorial.moved = false
 Tutorial.attacked = false
+Tutorial.chewing = 0
 Tutorial.turned = false
 Tutorial.target = {r = 1, g = 0, b = 0, a = 1}
-Tutorial.target.pos = {x = 300, y = 300, r = 75, d = 0}
+Tutorial.target.pos = {x = 300, y = 300, r = 75, d = 0, v = 1}
 Tutorial.tut = {r = 0, g = .5, b = .9, a = 1}
 
 local function target_left()
@@ -51,6 +52,9 @@ local function draw_protagonist()
   love.graphics.translate(Tutorial.pos.x, Tutorial.pos.y)
   love.graphics.rotate(Tutorial.direction + math.pi * 0.5)
   love.graphics.translate(-Tutorial.pos.x, -Tutorial.pos.y)
+  love.graphics.setColor(.7,.7,0,1)
+  love.graphics.print(".", Tutorial.pos.x, Tutorial.pos.y- 70)
+  love.graphics.setColor(.1,.4,.1,1)
   love.graphics.print("W", bouncy_x(0, Tutorial.forward.x), bouncy_y(1,Tutorial.forward.y))
   love.graphics.print("A", bouncy_x(1, Tutorial.left.x), bouncy_y(0, Tutorial.left.y))
   love.graphics.print("S", bouncy_x(0, Tutorial.back.x), bouncy_y(-1, Tutorial.back.y))
@@ -70,17 +74,15 @@ local function draw_living_tutorial()
   draw_energy()
   if Tutorial.playing then
     love.graphics.setColor(Tutorial.target.r, Tutorial.target.g, Tutorial.target.b, Tutorial.target.a)
-    love.graphics.print("E", Tutorial.target.pos.x, Tutorial.target.pos.y)
+    love.graphics.print("><>", Tutorial.target.pos.x, Tutorial.target.pos.y)
     love.graphics.setColor(Tutorial.tut.r, Tutorial.tut.g, Tutorial.tut.b, Tutorial.tut.a)
+    love.graphics.print("Score: "..math.floor(Tutorial.score), 10, 510)
   else
     love.graphics.setColor(Tutorial.tut.r, Tutorial.tut.g, Tutorial.tut.b, Tutorial.tut.a)
-    love.graphics.print("Move over the red letter", 50, 50)
-    love.graphics.print("and press 'e' to attack it!", 100, 100)
+    love.graphics.print("Move over the red fish", 50, 50)
+    love.graphics.print("and press 'e' to eat it!", 100, 100)
     love.graphics.setColor(Tutorial.target.r, Tutorial.target.g, Tutorial.target.b, Tutorial.target.a)
-    love.graphics.print("E", 300, 300)
-    love.graphics.setColor(Tutorial.tut.r, Tutorial.tut.g, Tutorial.tut.b, Tutorial.tut.a)
-    love.graphics.print(Tutorial.pos.x, 0, 400)
-    love.graphics.print(Tutorial.pos.y, 0, 450)
+    love.graphics.print("><>", 300, 300)
   end
   draw_protagonist()
 end
@@ -96,42 +98,49 @@ function draw_tutorial()
     love.graphics.print("(Y)es           (Q)uit", 200, 300)
     love.graphics.setColor(0,1,.5,1)
     if Tutorial.high_score < Tutorial.score then
-      love.graphics.print("Your score of "..math.floor(Tutorial.score).." beat the highscore of ", 50, 400)
-      love.graphics.print("beat the highscore of "..Tutorial.high_score.."!", 50, 450)
+      love.graphics.print("Your score of "..math.floor(Tutorial.score), 50, 400)
+      love.graphics.print("beat the highscore of "..math.floor(Tutorial.high_score).."!", 50, 450)
     else
       love.graphics.print("Score: "..math.floor(Tutorial.score), 300, 400)
     end
   end
 end
 
-
 local function update_bounce(dt)
+  local chew_frequency = 10
+  local chew_amplitude = 5
+  if Tutorial.chewing > 0 then
+    Tutorial.chewing = Tutorial.chewing - dt
+    chew_frequency = 5
+    chew_amplitude = 100
+  end
   local new_total = Tutorial.bounce.total + dt
   new_total = new_total % 20
   Tutorial.bounce.total = new_total
-  Tutorial.bounce.offs = 10 * math.sin(10 * Tutorial.bounce.total)
+  Tutorial.bounce.offs = chew_amplitude * math.sin(chew_frequency * Tutorial.bounce.total)
 end
 
+local function delta_x(speed, angle)
+  return speed * math.cos(angle)
+end
+local function delta_y(speed, angle)
+  return speed * math.sin(angle)
+end
 local function new_x(speed)
-  return Tutorial.pos.x + speed * math.cos(Tutorial.direction)
+  return Tutorial.pos.x + delta_x(speed, Tutorial.direction)
 end
-
 local function new_y(speed)
-  return Tutorial.pos.y + speed * math.sin(Tutorial.direction)
+  return Tutorial.pos.y + delta_y(speed, Tutorial.direction)
 end
-
 local function movement_energy_expenditure(dt)
   Tutorial.energy = Tutorial.energy - 1/2 * dt
 end
-
 local function turn_energy_expenditure(dt)
   Tutorial.energy = Tutorial.energy - 1/30 * dt
 end
-
 local function attack_energy_expenditure()
-  Tutorial.energy = Tutorial.energy - Tutorial.energy * 0.1
+  Tutorial.energy = Tutorial.energy - 0.1
 end
-
 local function update_energy(dt)
   Tutorial.energy = Tutorial.energy - 1/500 * dt
   if Tutorial.moved then
@@ -144,14 +153,16 @@ local function update_energy(dt)
   end
   if Tutorial.attacked then
     attack_energy_expenditure()
+    Tutorial.attacked = false
+  end
+  if Tutorial.chewing > 0 then
     if on_target() then
-      Tutorial.score = Tutorial.score + 0.25
-      Tutorial.energy = Tutorial.energy + 0.25
+      Tutorial.score = Tutorial.score + 1
+      Tutorial.energy = Tutorial.energy + 0.005
       if Tutorial.energy >= 1 then
         Tutorial.energy = 1
       end
     end
-    Tutorial.attacked = false
   end
 end
 
@@ -163,9 +174,23 @@ local function random_signed()
 end
 
 local function update_target(dt)
-  local delta_x = math.random()
-  Tutorial.target.pos.x = Tutorial.target.pos.x + random_signed()
-  Tutorial.target.pos.y = Tutorial.target.pos.y + random_signed()
+  if Tutorial.chewing > 0 and on_target() then
+    Tutorial.target.pos.d = Tutorial.target.pos.d + random_signed()
+  else
+    Tutorial.target.pos.d = Tutorial.target.pos.d + random_signed() * 0.1
+    Tutorial.target.pos.v = 1
+  end
+  if Tutorial.target.pos.x > love.graphics.getWidth() - 70 then
+    Tutorial.target.pos.d = Tutorial.target.pos.d - math.pi
+  elseif Tutorial.target.pos.x < 0 then
+    Tutorial.target.pos.d = Tutorial.target.pos.d + math.pi
+  elseif Tutorial.target.pos.y > love.graphics.getHeight() - 50 then
+    Tutorial.target.pos.d = Tutorial.target.pos.d - math.pi * 0.5
+  elseif Tutorial.target.pos.y < -30 then
+    Tutorial.target.pos.d = Tutorial.target.pos.d + math.pi * 0.5
+  end
+  Tutorial.target.pos.x = Tutorial.target.pos.x + delta_x(Tutorial.target.pos.v, Tutorial.target.pos.d)
+  Tutorial.target.pos.y = Tutorial.target.pos.y + delta_y(Tutorial.target.pos.v, Tutorial.target.pos.d)
 end
 
 function update_tutorial(dt)
@@ -206,7 +231,9 @@ function keypressed_tutorial(key)
       if not Tutorial.playing and on_target() then
         Tutorial.playing = true
       end
+      Tutorial.chewing = 4
       Tutorial.attacked = true
+      Tutorial.target.v = 5
     end
   else
     if key == "y" then
